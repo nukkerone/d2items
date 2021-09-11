f9118828b72bba1fdd8eca44deebd0fa543d4c5d
Gabriel Medina <gabi89.m@gmail.com>
2021-09-11 08:46:56 -0300

    Testing SSG for uniques page

--- pages/uniques/index.js
+++ pages/uniques/index.js
@@ -121,63 +121,6 @@ export default function Uniques({ uniqueitems }) {
               </div>
             </div>
           }}></CustomMasonry>
-          {/* {
-            items.map(item =>
-              <div key={item._id} className="col-lg-4 grid-item">
-                <div className="card mb-3">
-                  <div className="card-body">
-
-                    <Image
-                      src={'https://diablo2.io' + item.image.src}
-                      alt={item.name}
-                      width={item.image.width}
-                      height={item.image.height}
-                    />
-
-                    <h2>{item.name}</h2>
-                    <h3>{item.tier}</h3>
-                    <h4>{item.base}</h4>
-
-                    <br />
-
-                    {
-                      Object.entries(item).map(([key, val], i) => {
-                        // We avoid entries that are props
-                        const match = key.match(/(prop)[0-9]+/g);
-                        if (match && match.length > 0) {
-                          return null;
-                        }
-                        // We avoid the following fields
-                        if (['_id', 'name', 'tier', 'base', 'patch', 'only', 'image'].indexOf(key) >= 0) {
-                          return null;
-                        }
-                        // We print the stats 
-                        return <p className="stat" key={key}>{key.charAt(0).toUpperCase() + key.replace('_', ' ').slice(1)}: <span>{val}</span></p>
-                      })
-                    }
-
-                    <br />
-
-                    {
-                      Object.entries(item).map(([key, val], i) => {
-                        // Print the props
-                        const match = key.match(/(prop)[0-9]+/g);
-                        if (match && match.length > 0) {
-                          return <p className="property" key={i}>{val}</p>
-                        }
-                      })
-                    }
-
-                    <br />
-
-                    {item.patch ? <p className="patch">{item.patch}</p> : null}
-                    {item.only ? <p className="only">{item.only}</p> : null}
-                  </div>
-
-                </div>
-              </div>
-            )
-          } */}
         </div>
 
       </div>
@@ -188,7 +131,7 @@ export default function Uniques({ uniqueitems }) {
 }
 
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   const { db } = await connectToDatabase()
   const uniqueitems = await db.collection('unique_scrapped_normalized').find({}).limit(500).toArray();
 
