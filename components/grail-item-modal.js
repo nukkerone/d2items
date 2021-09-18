01d879b14b6c1b3d878c1629bc34d3a09afc4c95
Gabriel Medina <gabi89.m@gmail.com>
2021-09-18 06:31:35 -0300

    Grail item modal

--- components/grail-item-modal.js
+++ components/grail-item-modal.js
@@ -3,7 +3,7 @@ import { Modal, Button } from 'react-bootstrap';
 
 function GrailItemModal({ type, item, onHide }) {
   const [show, setShow] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   const [grailItem, setGrailItem] = useState(null);
 
   useEffect(() => {
@@ -16,24 +16,87 @@ function GrailItemModal({ type, item, onHide }) {
   }, [item, type]);
 
   const fetchGrailItem = async (item) => {
-    setLoading(true);
     console.log('Item ', item);
     const res = await fetch('/api/user/grail/' + type + '/' + item.slug, {
       method: 'GET',
     });
     const grailItem = await res.json();
     setGrailItem(grailItem);
-    setLoading(false);
+    setLoaded(true);
   }
 
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Body>
-        { loading &&
+        { !loaded &&
           <p>Loading</p>
         }
-        { !loading && item &&
-          <p>Item loaded</p>  
+        { loaded &&
+          <form>
+            <div className="mb-3">
+              <label htmlFor="character" className="form-label">Character</label>
+              <select class="form-select" name="character" id="character" aria-label="Character who found it">
+                <option value="Sorceress">Sorceress</option>
+                <option value="Barbarian">Barbarian</option>
+                <option value="Assasain">Assasain</option>
+                <option value="Druid">Druid</option>
+                <option value="Paladin">Paladin</option>
+                <option value="Amazon">Amazon</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="found-at" className="form-label">Where you found it?</label>
+              <input type="text" className="form-control" name="foundAt" id="fount-at" placeholder="Place where you found it" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="magic-find" className="form-label">Magic Find?</label>
+              <input type="text" className="form-control" name="magicFind" id="magic-find" placeholder="% of Magic find" />
+            </div>
+            <div className="mb-3">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="checkbox" name="perfect" id="perfect" value="1" />
+                <label className="form-check-label" htmlFor="perfect">Perfect</label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="checkbox" name="ethereal" id="ethereal" value="1" />
+                <label className="form-check-label" htmlFor="ethereal">Ethereal</label>
+              </div>
+            </div>
+            <div className="mb-3">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="difficulty" id="normal" />
+                <label className="form-check-label" htmlFor="normal">
+                  Normal
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="difficulty" id="nightmare" />
+                <label className="form-check-label" htmlFor="nightmare">
+                  Nightmare
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="difficulty" id="hell" checked />
+                <label className="form-check-label" htmlFor="hell">
+                  Hell
+                </label>
+              </div>
+            </div>
+            <div className="mb-3">
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="gameType" id="softcore" checked />
+                <label className="form-check-label" htmlFor="softcore">
+                  Softcore
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input className="form-check-input" type="radio" name="gameType" id="hardcore" />
+                <label className="form-check-label" htmlFor="hardcore">
+                  Hardcore
+                </label>
+              </div>
+            </div>
+          </form>
         }
       </Modal.Body>
       <Modal.Footer>
--- pages/api/user/grail/[...gid].js
+++ pages/api/user/grail/[...gid].js
@@ -18,8 +18,8 @@ const get = async (req, res) => {
     const grailItem = await db.collection('grailitems').findOne({ email: user.email, type, slug });
     console.log('Grail item', grailItem);
 
-    return res.status(200).json(grailItem);
+    return res.status(200).json(grailItem ?? 'null');
   } else {
-    return res.status(401).json(null);
+    return res.status(401).json('null');
   }
 }
