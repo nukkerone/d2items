0aee37495c5fb4b5811632de5a7567191d679ed0
Gabriel Medina <gabi89.m@gmail.com>
2021-10-19 09:00:03 -0300

    Indext redirection to uniques page

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ['diablo2.io'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/uniques',
+        permanent: true
+      }
+    ]
+  }
 }
